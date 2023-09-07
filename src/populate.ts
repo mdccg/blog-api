2c8f029eeab224dafddaa61336f448740b002e69
Matheus Daniel Cristal Comparotto Gomes <comparotto.js@gmail.com>
2023-09-06 23:28:45 -0400

    Adicionado script para popular o banco de dados

--- src/populate.ts
+++ src/populate.ts
@@ -1,12 +1,17 @@
+import { faker } from '@faker-js/faker';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 import { UserController } from './controllers/UserController';
-import 'reflect-metadata'
-import { DataSource } from 'typeorm'
-import { Post } from './entities/Post'
-import { User } from './entities/User'
+import { Post } from './entities/Post';
+import { User } from './entities/User';
+import { PostController } from './controllers/PostController';
 
-// const generatePost = (userId: string): Post => {
-//   const post = Post.createPost();
-// }
+const generatePost = (): Post => {
+  const title = faker.lorem.sentence();
+  const content = faker.lorem.paragraphs();
+  const post = Post.createPost(title, content);
+  return post;
+}
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
@@ -16,20 +21,26 @@ export const AppDataSource = new DataSource({
   entities: [User, Post],
   migrations: [],
   subscribers: [],
-})
+});
 
 AppDataSource.initialize()
   .then(async () => {
     const userController = new UserController();
-    
+    const postController = new PostController();
+
     let user: User;
 
     user = User.createUser('Matheus Daniel Cristal Comparotto Gomes', 'matheus.gomes@estudante.ifms.edu.br', '123');
     user = await userController.registerUser(user);
-    
-    console.log(user);
-  })
-  .catch((err) => {
-    console.error('Error during Data Source initialization', err)
-  });
+    user.posts = [];
 
+    const posts = Array(100000).fill(null).map(generatePost);
+
+    posts.forEach(async (post) => {
+      post.user = user;
+      post = await postController.save(post);
+      user.posts.push(post);
+    });
+  })
+  .catch((err) => console.error('Error during Data Source initialization', err))
+  .finally(() => console.log('ϟ Malfeito feito'));
